8303befb30b736e5f2f910c1a0573be8b9e01f37
jbleyel <jbleyel@gmx.net>
2024-09-25 10:54:23 +0200

    debug

--- lib/components/tuxtxtapp.cpp
+++ lib/components/tuxtxtapp.cpp
@@ -14,6 +14,7 @@ extern "C" void tuxtxt_start(int tpid, int demux);
 extern "C" int tuxtxt_stop();
 extern "C" void tuxtxt_close();
 extern "C" void tuxtxt_handlePressedKey(int key);
+extern "C" void tuxtxt_setdebug(bool debug);
 
 eAutoInitP0<eTuxtxtApp> init_eTuxtxtApp(eAutoInitNumbers::lowlevel, "Tuxtxt");
 eTuxtxtApp *eTuxtxtApp::instance = NULL;
@@ -74,6 +75,7 @@ void eTuxtxtApp::thread_finished()
 void eTuxtxtApp::initCache()
 {
 	tuxtxt_init();
+	tuxtxt_setdebug(true);
 }
 
 void eTuxtxtApp::freeCache()
