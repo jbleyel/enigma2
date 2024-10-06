aafe13e409c073fe0d12ea03aba89c211f9a6f4d
jbleyel <jbleyel@gmx.net>
2024-10-06 16:54:47 +0200

    hotplug

--- lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
+++ lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
@@ -5,6 +5,9 @@ from twisted.internet.protocol import Protocol, Factory
 
 from Plugins.Plugin import PluginDescriptor
 from Components.Harddisk import harddiskmanager
+from Screens.MessageBox import MessageBox
+from Tools.Notifications import AddPopup
+from Tools.Directories import fileReadLines
 
 # globals
 hotplugNotifier = []
@@ -20,53 +23,95 @@ def AudiocdAdded():
 
 
 def processHotplugData(self, v):
+	mode = v.get("mode")
 	print("[Hotplug.plugin.py]:", v)
 	action = v.get("ACTION")
-	device = v.get("DEVPATH")
-	physdevpath = v.get("PHYSDEVPATH")
-	media_state = v.get("X_E2_MEDIA_STATUS")
-	global audiocd
+	if mode == 1:
+		if action == "add":
+			ID_TYPE = v.get("ID_TYPE")
+			DEVTYPE = v.get("DEVTYPE")
+			if ID_TYPE == "disk" and DEVTYPE == "partition":
+				device = v.get("DEVPATH")
+				DEVNAME = v.get("DEVNAME")
+				ID_FS_TYPE = v.get("ID_FS_TYPE")
+				ID_BUS = v.get("ID_BUS")
+				ID_FS_UUID = v.get("ID_FS_UUID")
+				ID_MODEL = v.get("ID_MODEL")
+				ID_PART_ENTRY_SIZE = v.get("ID_PART_ENTRY_SIZE")
+				harddiskmanager.enumerateBlockDevices()
+				notFound = True
+				knownDevices = fileReadLines("/etc/udev/known_devices")
+				if knownDevices:
+					for device in knownDevices:
+						deviceData = device.split(":")
+						if len(deviceData) == 2 and deviceData[0] == ID_FS_UUID and deviceData[1]:
+							notFound = False
+							break
+				if notFound:
+					text = f"{ID_MODEL} - {DEVNAME}\n"
+					text = _("A new device has been pluged-in:\n%s") % text
+					text += _("Please goto Device Manager and choose what todo.")
+					AddPopup(text=text, type=MessageBox.TYPE_INFO, timeout=10)
+		elif action == "remove":
+			ID_TYPE = v.get("ID_TYPE")
+			DEVTYPE = v.get("DEVTYPE")
+			if ID_TYPE == "disk" and DEVTYPE == "partition":
+				#device = v.get("DEVNAME")
+				#harddiskmanager.removeHotplugPartition(device)
+				harddiskmanager.enumerateBlockDevices()
+		elif action == "ifup":
+			interface = v.get("INTERFACE")
+		elif action == "ifdown":
+			interface = v.get("INTERFACE")
+		elif action == "online":
+			state = v.get("STATE")
 
-	dev = device.split("/")[-1]
+	else:
+		device = v.get("DEVPATH")
+		physdevpath = v.get("PHYSDEVPATH")
+		media_state = v.get("X_E2_MEDIA_STATUS")
+		global audiocd
 
-	if action == "add":
-		error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugPartition(dev, physdevpath)
-	elif action == "remove":
-		harddiskmanager.removeHotplugPartition(dev)
-	elif action == "audiocdadd":
-		audiocd = True
-		media_state = "audiocd"
-		error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugAudiocd(dev, physdevpath)
-		print("[Hotplug.plugin.py] AUDIO CD ADD")
-	elif action == "audiocdremove":
-		audiocd = False
-		file = []
-		# Removing the invalid playlist.e2pls If its still the audio cd's list
-		# Default setting is to save last playlist on closing Mediaplayer.
-		# If audio cd is removed after Mediaplayer was closed,
-		# the playlist remains in if no other media was played.
-		if isfile("/etc/enigma2/playlist.e2pls"):
-			with open("/etc/enigma2/playlist.e2pls") as f:
-				file = f.readline().strip()
-		if file and ".cda" in file:
+		dev = device.split("/")[-1]
+
+		if action == "add":
+			error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugPartition(dev, physdevpath)
+		elif action == "remove":
+			harddiskmanager.removeHotplugPartition(dev)
+		elif action == "audiocdadd":
+			audiocd = True
+			media_state = "audiocd"
+			error, blacklisted, removable, is_cdrom, partitions, medium_found = harddiskmanager.addHotplugAudiocd(dev, physdevpath)
+			print("[Hotplug.plugin.py] AUDIO CD ADD")
+		elif action == "audiocdremove":
+			audiocd = False
+			file = []
+			# Removing the invalid playlist.e2pls If its still the audio cd's list
+			# Default setting is to save last playlist on closing Mediaplayer.
+			# If audio cd is removed after Mediaplayer was closed,
+			# the playlist remains in if no other media was played.
+			if isfile("/etc/enigma2/playlist.e2pls"):
+				with open("/etc/enigma2/playlist.e2pls") as f:
+					file = f.readline().strip()
+			if file and ".cda" in file:
+				try:
+					remove("/etc/enigma2/playlist.e2pls")
+				except OSError:
+					pass
+			harddiskmanager.removeHotplugPartition(dev)
+			print("[Hotplug.plugin.py] REMOVING AUDIOCD")
+		elif media_state is not None:
+			if media_state == "1":
+				harddiskmanager.removeHotplugPartition(dev)
+				harddiskmanager.addHotplugPartition(dev, physdevpath)
+			elif media_state == "0":
+				harddiskmanager.removeHotplugPartition(dev)
+
+		for callback in hotplugNotifier:
 			try:
-				remove("/etc/enigma2/playlist.e2pls")
-			except OSError:
-				pass
-		harddiskmanager.removeHotplugPartition(dev)
-		print("[Hotplug.plugin.py] REMOVING AUDIOCD")
-	elif media_state is not None:
-		if media_state == "1":
-			harddiskmanager.removeHotplugPartition(dev)
-			harddiskmanager.addHotplugPartition(dev, physdevpath)
-		elif media_state == "0":
-			harddiskmanager.removeHotplugPartition(dev)
-
-	for callback in hotplugNotifier:
-		try:
-			callback(dev, action or media_state)
-		except AttributeError:
-			hotplugNotifier.remove(callback)
+				callback(dev, action or media_state)
+			except AttributeError:
+				hotplugNotifier.remove(callback)
 
 
 class Hotplug(Protocol):
@@ -81,12 +126,16 @@ class Hotplug(Protocol):
 		if isinstance(data, bytes):
 			data = data.decode()
 		self.received += data
-		print("[Hotplug.plugin.py] complete", self.received)
 
 	def connectionLost(self, reason):
 		print("[Hotplug.plugin.py] connection lost!")
-		data = self.received.split("\0")[:-1]
 		v = {}
+		if "\n" in self.received:
+			data = self.received[:-1].split("\n")
+			v["mode"] = 1
+		else:
+			data = self.received.split("\0")[:-1]
+			v["mode"] = 0
 		for x in data:
 			i = x.find("=")
 			var, val = x[:i], x[i + 1:]
