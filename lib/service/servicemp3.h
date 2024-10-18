4c39c25c3ac53546115cf566ad6117a30b3ecdd4
jbleyel <jbleyel@gmx.net>
2024-10-18 07:36:17 +0200

    dvb subs

--- lib/service/servicemp3.h
+++ lib/service/servicemp3.h
@@ -349,6 +349,11 @@ private:
 	GstElement *m_gst_playbin;
 	GstTagList *m_stream_tags;
 	bool m_coverart;
+	subtitle_page *m_pages;
+	eSize m_display_size;
+	bool m_seen_eod;
+	pts_t m_show_time;
+	std::list<eDVBSubtitlePage> m_dvb_subtitle_pages;
 
 	eFixedMessagePump<ePtr<GstMessageContainer> > m_pump;
 
@@ -387,6 +392,13 @@ private:
 	pts_t m_prev_decoder_time;
 	int m_decoder_time_valid_state;
 
+	void subtitle_redraw_all();
+	void subtitle_reset();
+	void subtitle_redraw(int page_id);
+	int subtitle_process_pixel_data(subtitle_region *region, subtitle_region_object *object, int *linenr, int *linep, uint8_t *data);
+	void subtitle_process_line(subtitle_region *region, subtitle_region_object *object, int line, uint8_t *data, int len);
+
+	void pushDVBSubtitles(const eDVBSubtitlePage &p);
 	void pushSubtitles();
 	void pullSubtitle(GstBuffer *buffer);
 	void sourceTimeout();
