f2c18fd1d2edc3bfb3c3692ec13d069bf6cb9566
jbleyel <jbleyel@gmx.net>
2025-01-15 16:25:28 +0100

    test

--- lib/service/servicefs.cpp
+++ lib/service/servicefs.cpp
@@ -93,7 +93,7 @@ DEFINE_REF(eServiceFS);
 eServiceFS::eServiceFS(const char *path, const char *additional_extensions): path(path)
 {
 	m_list_valid = 0;
-	eDebug("[eServiceFS] path (%s)", path.c_str());
+	eDebug("[eServiceFS] path (%s)", path);
 	eDebug("[eServiceFS] additional_extensions (%s)", additional_extensions);
 	if (additional_extensions)
 	{
