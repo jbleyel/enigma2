d4817d0bcb00457cd2607c28cb6ddf5ad6d25e21
jbleyel <jbleyel@gmx.net>
2025-03-31 22:35:05 +0200

    master

--- lib/python/Components/Renderer/SingleEpgList.py
+++ lib/python/Components/Renderer/SingleEpgList.py
@@ -9,6 +9,8 @@ class SingleEpgList(Renderer, VariableText):
 
 	def __init__(self):
 		self.maxEvents = 4
+		self.minEvent = 1
+		self.ignoreNA = False
 		Renderer.__init__(self)
 		VariableText.__init__(self)
 		self.epgcache = eEPGCache.getInstance()
@@ -19,6 +21,16 @@ class SingleEpgList(Renderer, VariableText):
 			if attrib == "maxEvents":
 				self.maxEvents = int(value)
 				attribs.remove((attrib, value))
+			if attrib == "minEvent":
+				self.minEvent = int(value)
+				attribs.remove((attrib, value))
+			if attrib == "ignoreNA":
+				self.ignoreNA = value.toLower() in ("true", "1", "enabled", "yes", "on", attrib.toLower())
+				attribs.remove((attrib, value))
+		if self.maxEvents < self.minEvent:
+			self.minEvent = 1
+		if self.maxEvents > 10:
+			self.maxEvents = 10
 		self.skinAttributes = attribs
 		return Renderer.applySkin(self, desktop, parent)
 
@@ -28,15 +40,15 @@ class SingleEpgList(Renderer, VariableText):
 		if event:
 			service = self.source.service
 			if self.epgcache:
-				events = self.epgcache.lookupEvent(["IBDCT", (service.toString(), 0, -1, -1)])
+				events = self.epgcache.lookupEvent(['IBDCTM', (service.toString(), 0, -1, -1)])
 				if events:
 					eventCount = 0
 					for event in events:
-						if eventCount > 0:
+						if eventCount > 0 and eventCount >= self.minEvent:
 							if event[4]:
 								localTime = localtime(event[1])
 								text.append(f"{localTime[3]:02d}:{localTime[4]:02d} {event[4]}")
-							else:
+							elif not self.ignoreNA:
 								text.append(_("N/A"))
 
 						eventCount += 1
