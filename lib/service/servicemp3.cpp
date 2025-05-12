01753afc30dfeb0ef15b481e391a96422cd57a60
jbleyel <jbleyel@gmx.net>
2025-05-12 16:30:20 +0200

    test

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -3260,18 +3260,37 @@ std::string eServiceMP3::downloadPlaylist(const gchar *uri)
 {
     eDebug("[eServiceMP3] Downloading HLS playlist: %s", uri);
 
+    // Erstelle die Pipeline
     GstElement *src = gst_element_factory_make("souphttpsrc", "playlist_source");
-    g_object_set(src, "location", uri, NULL);
-
     GstElement *sink = gst_element_factory_make("fakesink", "sink");
+    GstElement *pipeline = gst_pipeline_new("playlist_pipeline");
+
+    // Überprüfe, ob alle Elemente erfolgreich erstellt wurden
+    if (!src || !sink || !pipeline)
+    {
+        eDebug("[eServiceMP3] Failed to create GStreamer elements");
+        if (src) gst_object_unref(src);
+        if (sink) gst_object_unref(sink);
+        if (pipeline) gst_object_unref(pipeline);
+        return "";
+    }
+
+    g_object_set(src, "location", uri, NULL);
     g_object_set(sink, "sync", FALSE, "async", FALSE, "signal-handoffs", TRUE, NULL);
 
-    GstElement *pipeline = gst_pipeline_new("playlist_pipeline");
     gst_bin_add_many(GST_BIN(pipeline), src, sink, NULL);
-    gst_element_link(src, sink);
+    if (!gst_element_link(src, sink))
+    {
+        eDebug("[eServiceMP3] Failed to link src and sink");
+        gst_object_unref(pipeline);
+        gst_object_unref(src);
+        gst_object_unref(sink);
+        return "";
+    }
 
     std::ostringstream playlist_stream;
 
+    // Signal für "handoff" verbinden
     g_signal_connect(sink, "handoff", G_CALLBACK(+[](GstElement *, GstBuffer *buffer, GstPad *, gpointer user_data) {
         std::ostringstream *playlist_stream = static_cast<std::ostringstream *>(user_data);
         GstMapInfo map;
@@ -3283,56 +3302,65 @@ std::string eServiceMP3::downloadPlaylist(const gchar *uri)
     }),
                      &playlist_stream);
 
+    // GMainLoop erstellen
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+
+    // EOS-Handler hinzufügen
+    GstBus *bus = gst_element_get_bus(pipeline);
+    g_signal_connect(bus, "message", G_CALLBACK(+[](GstBus *, GstMessage *msg, gpointer user_data) -> gboolean {
+        GMainLoop *loop = static_cast<GMainLoop *>(user_data);
+
+        switch (GST_MESSAGE_TYPE(msg))
+        {
+        case GST_MESSAGE_EOS:
+            eDebug("[eServiceMP3] End of stream reached");
+            g_main_loop_quit(loop);
+            break;
+
+        case GST_MESSAGE_ERROR:
+        {
+            GError *err = NULL;
+            gchar *debug_info = NULL;
+            gst_message_parse_error(msg, &err, &debug_info);
+            eDebug("[eServiceMP3] Error received: %s", err->message);
+            eDebug("[eServiceMP3] Debugging information: %s", debug_info ? debug_info : "none");
+            g_clear_error(&err);
+            g_free(debug_info);
+            g_main_loop_quit(loop);
+            break;
+        }
+
+        default:
+            break;
+        }
+        return TRUE;
+    }),
+                     loop);
+
+    // Starte die Pipeline
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-    GstBus *bus = gst_element_get_bus(pipeline);
-    gboolean eos_reached = FALSE;
+    // Warte auf EOS
+    g_main_loop_run(loop);
 
-    while (!eos_reached)
-    {
-        GstMessage *msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-                                                     (GstMessageType)(GST_MESSAGE_EOS | GST_MESSAGE_ERROR));
-
-        if (msg)
-        {
-            switch (GST_MESSAGE_TYPE(msg))
-            {
-            case GST_MESSAGE_EOS:
-				eTrace("[eServiceMP3] End of stream reached");
-                eos_reached = TRUE;
-                break;
-
-            case GST_MESSAGE_ERROR:
-            {
-                GError *err = NULL;
-                gchar *debug_info = NULL;
-                gst_message_parse_error(msg, &err, &debug_info);
-                eDebug("[eServiceMP3] Error received from element %s: %s", GST_OBJECT_NAME(msg->src), err->message);
-                eDebug("[eServiceMP3] Debugging information: %s", debug_info ? debug_info : "none");
-                g_clear_error(&err);
-                g_free(debug_info);
-                eos_reached = TRUE;
-                break;
-            }
-
-            default:
-                break;
-            }
-            gst_message_unref(msg);
-        }
-    }
-
+    // GMainLoop und Bus freigeben
+    g_main_loop_unref(loop);
     gst_object_unref(bus);
 
+    // Pipeline stoppen und freigeben
     gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
 
+    // Playlist-Daten als String extrahieren
     std::string playlist_data = playlist_stream.str();
     eTrace("[eServiceMP3] Complete Playlist data: %s", playlist_data.c_str());
 
+    // Ressourcen freigeben
+    gst_object_unref(src);
+    gst_object_unref(sink);
+    gst_object_unref(pipeline);
+
     return playlist_data;
 }
-
 // #EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID="textstream",LANGUAGE="de",NAME="German",DEFAULT=YES,AUTOSELECT=YES,URI="<LINK>"
 
 void eServiceMP3::parseHlsPlaylist(const std::string &playlist)
@@ -3379,14 +3407,12 @@ void eServiceMP3::parseHlsPlaylist(const std::string &playlist)
 				subtitleStream subs;
 				subs.language_code = language;
 				subs.title = name;
-//				subs.uri = uri;
+				subs.uri = uri;
 				subs.type = stVTT;
 				subtitleStreams_temp.emplace_back(subs);
             }
         }
     }
-
-//	return
 
 	bool hasChanges = m_subtitleStreams.size() != subtitleStreams_temp.size() || std::equal(m_subtitleStreams.begin(), m_subtitleStreams.end(), subtitleStreams_temp.begin());
 
