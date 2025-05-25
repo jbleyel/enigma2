c7c9a5b86c72f4512e54bb75c2075d2194f67cc9
jbleyel <jbleyel@gmx.net>
2025-05-25 19:08:12 +0200

    dash

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -972,10 +972,7 @@ eServiceMP3::eServiceMP3(eServiceReference ref):
 		g_object_set(G_OBJECT(m_gst_source),
 			"uri", uri,
 			"connection-speed", (guint64)4495000,
-			"download-buffering", TRUE,
-			"buffer-duration", (gint64)(10LL * GST_SECOND),    // 10s Buffer
 			"buffer-size", (guint64)(32LL * 1024LL * 1024LL),  // 32MB Buffer
-	        "use-buffering", TRUE,
 			NULL);
 
 		g_free(uri);
