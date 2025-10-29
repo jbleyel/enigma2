09a2ccd50a700d3504488330fd28c40415340ce6
jbleyel <jbleyel@gmx.net>
2025-10-29 22:21:51 +0100

    test

--- lib/gdi/gpixmap.cpp
+++ lib/gdi/gpixmap.cpp
@@ -2107,19 +2107,6 @@ void gPixmap::blit(const gPixmap& src, const eRect& _pos, const gRegion& clip, i
 		Stopwatch s;
 #endif
 
-#ifdef FORCE_NO_ACCELERATION_SCALE
-		if (accel && (flag & blitScale)) {
-			eDebug("[gPixmap] forcing no acceleration for scaling blit W %d / %d", src.size().width(), srcarea.width());
-			eDebug("[gPixmap] forcing no acceleration for scaling blit H %d / %d", src.size().height(), srcarea.height());
-			// Reset width in case of round issue
-			if (src.size().width() != srcarea.width())
-				srcarea.setWidth(src.size().width());
-
-			// Reset height in case of round issue
-			if (src.size().height() != srcarea.height())
-				srcarea.setHeight(src.size().height());
-		}
-#endif
 		if (accel) {
 			flag &= 7; // remove all flags except the blit flags
 			// eDebug("[gPixmap] accel flag %d / area (%d,%d,%d,%d) / srcarea (%d,%d,%d,%d)", flag, area.left(),
