d12ca70fb11b2d1a88ab4897309fcbf0bebf591f
jbleyel <jbleyel@gmx.net>
2026-06-18 21:34:17 +0200

    toast

--- lib/python/Screens/Toast.py
+++ lib/python/Screens/Toast.py
@@ -1,6 +1,5 @@
 from enigma import ePoint, gRGB, eSize, eTimer
 
-from Components.config import config, ConfigSelection
 from Components.Label import Label
 from Screens.Screen import Screen
 
@@ -14,9 +13,9 @@ class ToastScreen(Screen):
 	</screen>"""
 
 	ICON_CHARS = {
-		0: chr(59658),  # TYPE_INFO
-		1: chr(59659),  # TYPE_WARNING
-		2: chr(59657),  # TYPE_ERROR
+		0: "\uE90A",  # TYPE_INFO
+		1: "\uEA59",  # TYPE_WARNING
+		2: "\uEA56",  # TYPE_ERROR
 	}
 
 	def __init__(self, session):
@@ -24,44 +23,44 @@ class ToastScreen(Screen):
 		self["border"] = Label()
 		self["icon"] = Label()
 		self["text"] = Label()
-		self.timer = eTimer()
-		self.timer.callback.append(self.dohide)
-		self.fadeTimer = eTimer()
-		self.fadeTimer.callback.append(self.fade)
+		self._timer = eTimer()
+		self._timer.callback.append(self._dohide)
+		self._fadeTimer = eTimer()
+		self._fadeTimer.callback.append(self._doFade)
+		self._fadeSpeed = 1  # 2
 
 	def showToast(self, text, toasttype, timeout):
-		self.foregroundColor = {
+		self._foregroundColor = {
 			Toast.TYPE_INFO: (255, 255, 255),  # white
 			Toast.TYPE_WARNING: (0, 0, 0),  # black
 			Toast.TYPE_ERROR: (255, 255, 255)  # white
 		}.get(toasttype, (255, 255, 255))
 
-		self.backgroundColor = {
+		self._backgroundColor = {
 			Toast.TYPE_INFO: (0, 0, 0),  # black
 			Toast.TYPE_WARNING: (255, 165, 0),  # orange
 			Toast.TYPE_ERROR: (255, 0, 0)  # red
 		}.get(toasttype, (0, 0, 0))
 
-		self["icon"].setText(self.ICON_CHARS.get(toasttype, chr(59658)))
+		self["icon"].setText(self.ICON_CHARS.get(toasttype, ""))
 		self["text"].setText(text)
-		self.timer.start(timeout * 1000)
-		self.fade = config.usage.toastFade.value
+		self._timer.start(timeout * 1000)
 
 		BORDER_PAD = 18
 		GAP = 14
 		SCREEN_MARGIN = 60
 
-		self.iconSize = self["icon"].instance.calculateSize()
-		iconW = self.iconSize.width()
-		iconH = self.iconSize.height()
+		self._iconSize = self["icon"].instance.calculateSize()
+		iconW = self._iconSize.width()
+		iconH = self._iconSize.height()
 
 		screenW = self.instance.size().width()
 		maxTextW = screenW - SCREEN_MARGIN * 2 - BORDER_PAD * 2 - iconW - GAP
 		self["text"].instance.resize(eSize(maxTextW, self.instance.size().height()))
-		self.textSize = self["text"].instance.calculateSize()
+		self._textSize = self["text"].instance.calculateSize()
 
-		textW = self.textSize.width()
-		textH = self.textSize.height()
+		textW = self._textSize.width()
+		textH = self._textSize.height()
 		contentH = max(iconH, textH)
 		borderW = BORDER_PAD * 2 + iconW + GAP + textW
 		borderH = contentH + BORDER_PAD * 2
@@ -76,19 +75,15 @@ class ToastScreen(Screen):
 		self["text"].instance.resize(eSize(textW, textH))
 		self["text"].instance.move(ePoint(startX + BORDER_PAD + iconW + GAP, posY + BORDER_PAD + (contentH - textH) // 2))
 
-		self.fadeIn = True
-		if self.fade > 0:
-			self.alpha = 255
-			self._applyColors()
-			self.fadeTimer.start(50 * self.fade)
-		else:
-			self.alpha = 0
-			self._applyColors()
+		self._fadeIn = True
+		self._alpha = 255
+		self._applyColors()
+		self._fadeTimer.start(50 * self._fadeSpeed)
 		self.show()
 
 	def _applyColors(self):
-		fg = gRGB(*(self.foregroundColor[0], self.foregroundColor[1], self.foregroundColor[2], self.alpha))
-		bg = gRGB(*(self.backgroundColor[0], self.backgroundColor[1], self.backgroundColor[2], self.alpha))
+		fg = gRGB(*(self._foregroundColor[0], self._foregroundColor[1], self._foregroundColor[2], self._alpha))
+		bg = gRGB(*(self._backgroundColor[0], self._backgroundColor[1], self._backgroundColor[2], self._alpha))
 		self["border"].instance.setBackgroundColor(bg)
 		self["border"].instance.setWidgetBorderColor(fg)
 		self["icon"].instance.setBackgroundColor(bg)
@@ -96,32 +91,29 @@ class ToastScreen(Screen):
 		self["text"].instance.setBackgroundColor(bg)
 		self["text"].instance.setForegroundColor(fg)
 
-	def fade(self):
-		if self.fadeIn:
-			self.alpha -= 5
-			if self.alpha <= 0:
-				self.alpha = 0
-				self.fadeTimer.stop()
-				self.fadeIn = False
+	def _doFade(self):
+		if self._fadeIn:
+			self._alpha -= 5
+			if self._alpha <= 0:
+				self._alpha = 0
+				self._fadeTimer.stop()
+				self._fadeIn = False
 		else:
-			self.alpha += 5
-			if self.alpha >= 255:
-				self.alpha = 255
-				self.fadeTimer.stop()
+			self._alpha += 5
+			if self._alpha >= 255:
+				self._alpha = 255
+				self._fadeTimer.stop()
 				self.hide()
 		self._applyColors()
 
-	def dohide(self):
-		self.timer.stop()
-		self.fadeIn = False
-		if self.fade > 0:
-			self.fadeTimer.start(50 * self.fade)
-		else:
-			self.hide()
+	def _dohide(self):
+		self._timer.stop()
+		self._fadeIn = False
+		self._fadeTimer.start(50 * self._fadeSpeed)
 
 	def forceHide(self):
-		self.fadeTimer.stop()
-		self.timer.stop()
+		self._fadeTimer.stop()
+		self._timer.stop()
 		self.hide()
 
 
@@ -136,37 +128,32 @@ class Toast:
 			print("[Toast] Error: Only one Toast instance is allowed!")
 		else:
 			Toast.instance = self
-			self.dialog = session.instantiateDialog(ToastScreen)
-			self.dialog.hide()
-			self.queue = []
-			self.nextTimer = eTimer()
-			self.nextTimer.callback.append(self._showNext)
-			self.dialog.onHide.append(self._scheduleNext)
-			config.usage.toastFade = ConfigSelection(default=0, choices=[
-				(0, _("Off")),
-				(1, _("Fast")),
-				(2, _("Slow"))
-			])
+			self._dialog = session.instantiateDialog(ToastScreen)
+			self._dialog.hide()
+			self._queue = []
+			self._nextTimer = eTimer()
+			self._nextTimer.callback.append(self._showNext)
+			self._dialog.onHide.append(self._scheduleNext)
 
 	def showToast(self, text, toasttype, timeout):
 		timeout = max(3, min(timeout, 10))  # Minimum 3 maximum 10
-		self.queue.append((text, toasttype, timeout))
-		if not self.dialog.shown and not self.nextTimer.isActive():
+		self._queue.append((text, toasttype, timeout))
+		if not self._dialog.shown and not self._nextTimer.isActive():
 			self._showNext()
 
 	def _scheduleNext(self):
-		if self.queue:
-			self.nextTimer.start(1000, True)  # 1000 ms Pause
+		if self._queue:
+			self._nextTimer.start(1000, True)  # 1000 ms Pause
 
 	def _showNext(self):
-		self.nextTimer.stop()
-		if not self.dialog.shown and self.queue:
-			text, toasttype, timeout = self.queue.pop(0)
-			self.dialog.showToast(text, toasttype, timeout)
+		self._nextTimer.stop()
+		if not self._dialog.shown and self._queue:
+			text, toasttype, timeout = self._queue.pop(0)
+			self._dialog.showToast(text, toasttype, timeout)
 
 	def doShutdown(self):
-		self.nextTimer.stop()
-		if self.queue:
-			self.queue = None
-		if self.dialog.shown:
-			self.dialog.forceHide()
+		self._nextTimer.stop()
+		if self._queue:
+			self._queue = None
+		if self._dialog.shown:
+			self._dialog.forceHide()
