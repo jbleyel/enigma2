5fd84c8e877fa6f7f39793488db935836ce1b224
jbleyel <jbleyel@gmx.net>
2026-07-05 15:38:34 +0200

    * update skin * fix formatNetworkSpeed

--- lib/python/Tools/Conversions.py
+++ lib/python/Tools/Conversions.py
@@ -58,9 +58,7 @@ def scaleNumber(number, style="Si", suffix="B", format="%.3f"):
 
 
 def formatNetworkSpeed(speedMbit: int) -> str:
-	if speedMbit >= 1000:
-		return f"{speedMbit // 1000} Gbit/s"
-	return f"{speedMbit} Mbit/s"
+	return f"{speedMbit / 1000:g} Gbit/s" if speedMbit >= 1000 else f"{speedMbit} Mbit/s"
 
 
 class UnitMultipliers:
